2be6aedd2ad0f3086e423e8447ef24e32c25a25e
Ronald Nsereko <rownee2001@gmail.com>
2019-08-30 12:33:49 +0300

    Updates

---
+++ precache-manifest.2363a9ae682114b973ccc48b9798ae28.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a3a138c684cfced2853f55749cd2188",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "11a28114e55b76232c76",
+    "url": "/monsters-rolodex/static/css/main.f9dd4431.chunk.css"
+  },
+  {
+    "revision": "ebdf587097504289c06e",
+    "url": "/monsters-rolodex/static/js/2.3adefb50.chunk.js"
+  },
+  {
+    "revision": "11a28114e55b76232c76",
+    "url": "/monsters-rolodex/static/js/main.020f580d.chunk.js"
+  },
+  {
+    "revision": "3eb51d48b619f371fccd",
+    "url": "/monsters-rolodex/static/js/runtime~main.877b8247.js"
+  }
+]);
